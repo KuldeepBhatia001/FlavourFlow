7622fefd83aafedb341e535d10267838b593a6bb
sanjit <sss761@uowmail.edu.au>
2024-05-18 20:47:29 +1000

    .

--- flavourflow_app/models.py
+++ flavourflow_app/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Restaurant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='restaurant')
     name = models.CharField(max_length=500)
     phone = models.CharField(max_length=10)
-    address = models.CharField(max_length=500)
     abn = models.CharField(max_length=11, unique=True)
     category = models.CharField(max_length=255)
     logo = models.ImageField(upload_to='restaurant_logo/')
+
+
 class Customer(models.Model):
+    payment_method = models.CharField(max_length=50)
+    is_member = models.BooleanField(default=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
     phone = models.CharField(max_length=10)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
@@ -20,11 +26,13 @@ class Address(models.Model):
     country = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='address', blank=True, null=True)
 
+
 class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     is_active = models.BooleanField(default=True)
+
 
 class MenuItem(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
@@ -34,6 +42,7 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to='menu_item_images/', blank=True, null=True)
     is_available = models.BooleanField(default=True)
 
+
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer_orders')
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
@@ -42,12 +51,13 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     prepared_at = models.DateTimeField(blank=True, null=True)
 
-    def save(self, *args, **kwargs): # to use when admin editing order, maybe user functionality? idk
+    def save(self, *args, **kwargs):   # to use when admin editing order, maybe user functionality? idk
         if self.pk:  # checking if the order already exists | should ideally be prevented within cart logic
             original_order = Order.objects.get(pk=self.pk)
             if original_order.restaurant != self.restaurant:
                 raise ValueError("Order items must belong to the same restaurant.")
         super().save(*args, **kwargs)
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -55,10 +65,11 @@ class OrderItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+
 class Payment(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=5, decimal_places=2)
-    payment_method = models.CharField(max_length=50)
+    payment_method = models.ForeignKey(Customer, on_delete=models.CASCADE)
     PAYMENT_STATUS_CHOICES = (
         ('pending', 'Pending'),
         ('success', 'Success'),
