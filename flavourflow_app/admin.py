47e2cdfa0e97d8835776c051fc7cd027ecfdb986
sanjit <sss761@uowmail.edu.au>
2024-05-20 18:54:10 +1000

    Updated functionality according to Figma

--- flavourflow_app/admin.py
+++ flavourflow_app/admin.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from .models import Restaurant, Customer, Address, Menu, MenuItem, Order, OrderItem, Payment
+from .models import Restaurant, Customer, Menu, MenuItem, Order, OrderItem, Transaction
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
-admin.site.register(Address)
 admin.site.register(Menu)
 admin.site.register(MenuItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-admin.site.register(Payment)
+admin.site.register(Transaction)
