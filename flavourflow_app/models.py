0d63afc07d651cd81378080351d0f8d5319542eb
sanjit <sss761@uowmail.edu.au>
2024-05-20 18:52:27 +1000

    Updated functionality according to Figma

--- flavourflow_app/models.py
+++ flavourflow_app/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Customer(models.Model):
+    payment_method = models.CharField(max_length=50)
+    is_member = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    phone = models.CharField(max_length=10)
+
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=4)
+
+
 class Restaurant(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='restaurant')
     name = models.CharField(max_length=500)
@@ -10,21 +22,10 @@ class Restaurant(models.Model):
     category = models.CharField(max_length=255)
     logo = models.ImageField(upload_to='restaurant_logo/')
 
-
-class Customer(models.Model):
-    payment_method = models.CharField(max_length=50)
-    is_member = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
-    phone = models.CharField(max_length=10)
-
-
-class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=50)
     postcode = models.CharField(max_length=4)
-    country = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='address', blank=True, null=True)
 
 
 class Menu(models.Model):
@@ -36,6 +37,8 @@ class Menu(models.Model):
 
 class MenuItem(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
@@ -46,28 +49,26 @@ class MenuItem(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customer_orders')
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
     status = models.CharField(max_length=10, default='pending')
     total_price = models.DecimalField(max_digits=5, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     prepared_at = models.DateTimeField(blank=True, null=True)
-
-    def save(self, *args, **kwargs):   # to use when admin editing order, maybe user functionality? idk
-        if self.pk:  # checking if the order already exists | should ideally be prevented within cart logic
-            original_order = Order.objects.get(pk=self.pk)
-            if original_order.restaurant != self.restaurant:
-                raise ValueError("Order items must belong to the same restaurant.")
-        super().save(*args, **kwargs)
-
+    cust_rating = models.DecimalField(max_digits=5, decimal_places=1)
+    delivery = models.BooleanField(default=True)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+
     quantity = models.PositiveIntegerField(default=1)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    notes = models.CharField(max_length=255)
 
 
-class Payment(models.Model):
+class Transaction(models.Model):
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
+
     amount = models.DecimalField(max_digits=5, decimal_places=2)
     payment_method = models.ForeignKey(Customer, on_delete=models.CASCADE)
     PAYMENT_STATUS_CHOICES = (
@@ -77,3 +78,17 @@ class Payment(models.Model):
     )
     payment_status = models.CharField(max_length=20, choices=PAYMENT_STATUS_CHOICES, default='pending')
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class Analytics(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='monthly_analytics')
+    month = models.DateField()  # Store the first day of the month
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    cust_views = models.IntegerField(default=0)
+    avg_ticket = models.DecimalField(max_digits=8, decimal_places=2)
+    total_sales = models.IntegerField(default=0)
+    class Meta:
+        unique_together = ('restaurant', 'month')
+
+class Favourites(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order = models.OneToOneField(Restaurant, on_delete=models.CASCADE)
