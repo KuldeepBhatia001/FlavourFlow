a0b9c88a51eeb962c574dab5d1764dd173341a4f
sanjit <sss761@uowmail.edu.au>
2024-05-21 12:18:30 +1000

    JSONField migrations

--- flavourflow_app/admin.py
+++ flavourflow_app/admin.py
@@ -1,12 +1,11 @@
 
 from django.contrib import admin
-from .models import Restaurant, Customer, Menu, Meal, Order, OrderItem, Transaction, Analytics, ItemList
+from .models import Restaurant, Customer, Menu, Meal, Order, Transaction, Analytics, ItemList
 
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Menu)
 admin.site.register(Meal)
 admin.site.register(Order)
-admin.site.register(OrderItem)
 admin.site.register(Transaction)
 admin.site.register(Analytics)
