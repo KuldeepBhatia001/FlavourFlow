0b9e6d68e633141182004efa3d204d4f3770d074
Soham <sohamverma26@gmail.com>
2024-05-21 22:29:32 +1000

    forms fix

--- flavourflow_app/forms.py
+++ flavourflow_app/forms.py
@@ -2,46 +2,56 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from .models import *
+
 class UserForm(forms.ModelForm):
     email = forms.CharField(max_length=100, required=True)
-    password = forms.CharField(widget=forms.PasswordInput())
 
-    class Meta:
-        model = Customer
-        fields = ("username", "password", "first_name", "last_name", "email")
-
-class UserFormForEdit(forms.ModelForm):
-    email = forms.CharField(max_length=100, required=True)
-
-    class Meta:
-        model = Customer
-        fields = ("first_name", "last_name", "email")
-
-class RestaurantForm(forms.ModelForm):
-    class Meta:
-        model = Restaurant
-        fields = ("name", "phone", "address", "logo")
 
 class MenuForm(forms.ModelForm):
     class Meta:
         model = Menu
         exclude = ("restaurant",)
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+# class UserForm(forms.ModelForm):
+#     email = forms.CharField(max_length=100, required=True)
+#     password = forms.CharField(widget=forms.PasswordInput())
 
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+#     class Meta:
+#         model = Customer
+#         fields = ("phone", "payment_method", "is_member", "street", "city", "state", "postcode")
+
+# class UserFormForEdit(forms.ModelForm):
+#     email = forms.CharField(max_length=100, required=True)
+
+#     class Meta:
+#         model = Customer
+#         fields = ("user.first_name", "user.last_name")
+
+# class RestaurantForm(forms.ModelForm):
+#     class Meta:
+#         model = Restaurant
+#         fields = ("name", "phone", "address", "logo")
+
+# class MenuForm(forms.ModelForm):
+#     class Meta:
+#         model = Menu
+#         exclude = ("restaurant",)
+
+# class UserRegisterForm(UserCreationForm):
+#     email = forms.EmailField()
+
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
 
 
-class PaymentForm(forms.Form):
-    # Payment fields as previously defined
-    card_number = forms.CharField(max_length=16)
-    expiration_date = forms.CharField(max_length=5)
-    cvv = forms.CharField(max_length=3)
+# class PaymentForm(forms.Form):
+#     # Payment fields as previously defined
+#     card_number = forms.CharField(max_length=16)
+#     expiration_date = forms.CharField(max_length=5)
+#     cvv = forms.CharField(max_length=3)
 
-class DeliveryForm(forms.ModelForm):
-    class Meta:
-        model = Delivery
-        fields = ['delivery_location', 'delivery_option']
+# class DeliveryForm(forms.ModelForm):
+#     class Meta:
+#         model = Delivery
+#         fields = ['delivery_location', 'delivery_option']
