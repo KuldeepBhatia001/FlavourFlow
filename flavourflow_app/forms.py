47653cb7b16e0380ee56d455af32a59adebfb307
Soham <sohamverma26@gmail.com>
2024-05-21 21:16:32 +1000

    forms menu fix

--- flavourflow_app/forms.py
+++ flavourflow_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import User, Restaurant, Customer, Address, Menu, MenuItem,  Order, OrderItem, Payment
+from .models import *
 
 class UserForm(forms.ModelForm):
     email = forms.CharField(max_length=100, required=True)
@@ -25,7 +25,7 @@ class RestaurantForm(forms.ModelForm):
 
 class MenuForm(forms.ModelForm):
     class Meta:
-        model = MenuItem
+        model = Menu
         exclude = ("restaurant",)
 
 class UserRegisterForm(UserCreationForm):
