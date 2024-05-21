60b23ff58f005adc54f33663d9abb6493a82ed76
Soham <sohamverma26@gmail.com>
2024-05-21 21:02:59 +1000

    db completed, ready to be worked on with sample db.sqlite with test data

--- flavourflow_app/admin.py
+++ flavourflow_app/admin.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
-from .models import Restaurant, Customer, Menu, Meal, Order, Transaction, Analytics, ItemList
+from .models import *
 
-admin.site.register(Restaurant)
 admin.site.register(Customer)
+admin.site.register(Restaurant)
+admin.site.register(Item)
 admin.site.register(Menu)
-admin.site.register(Meal)
 admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShoppingCart)
 admin.site.register(Transaction)
 admin.site.register(Analytics)
